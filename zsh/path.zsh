f217f34aa35baffd380ae9a284992ee02866ca25
Gustaf Barkeling <gbarkeling@gmail.com>
2020-09-03 17:32:05 +0100

    Update path.zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,13 +4,6 @@ fpath=($fpath $DOTFILES/zsh/fpath)
 if [ ! "$PATH_LOADED" = "true" ]; then
     # We want to extend path once
     export EDITOR="$(which code)"
-    
-    export LANG=en_GB.UTF-8 # Default language
-    export LC_ALL=en_GB.UTF-8
-    
-    if [[ $commands[javac] ]]; then
-        export JAVA_HOME="$(dirname $(dirname $(realpath $(which javac))))"
-    fi
 
     # Add custom bin files
     if [ -d "$HOME/bin" ]; then export PATH="$HOME/bin:$PATH"; fi
