18abdb51d45c22e38fee7ab9ed5570cf0bad847e
Gustaf Barkeling <gbarkeling@gmail.com>
2020-09-03 17:56:57 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,6 +45,7 @@ _setup_cuda() {
     echo ">> SETTING CUDA <<"
     sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
     sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+    sudo apt-get update
     sudo apt-get install -y cuda-toolkit-11-0
     echo ">> DONE SETTING CUDA <<"
 }
